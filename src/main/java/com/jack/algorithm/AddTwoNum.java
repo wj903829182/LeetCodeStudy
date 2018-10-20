e6679f11a4a90bd7f5416a0e318eda2c75ecf5cd
jack <wj903829182@sina.cn>
2018-10-20 10:06:45 +0800

    两个数相加

--- src/main/java/com/jack/algorithm/AddTwoNum.java
+++ src/main/java/com/jack/algorithm/AddTwoNum.java
@@ -9,14 +9,47 @@ package com.jack.algorithm;
  */
 public class AddTwoNum {
     //题目描述：https://leetcode-cn.com/problems/add-two-numbers/description/
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return null;
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null,p=null;
+        ListNode p1 =l1;
+        ListNode p2 =l2;
+        int b = 0;
+        int sum = 0;
+        while (p1 != null || p2 != null || b>0) {
+            int num1 =0;
+            int num2 =0;
+            sum = 0;
+            if (p1 != null && p2 !=null) {
+                num1= p1.val;
+                p1 = p1.next;
+                num2= p2.val;
+                p2 = p2.next;
+            } else if (p1 != null && p2 == null) {
+                num1= p1.val;
+                p1 = p1.next;
+            }else if (p1 == null && p2 != null) {
+                num1= p2.val;
+                p2 = p2.next;
+            }
+            sum = (num1+num2+b)%10;
+            b = (num1+num2+b)/10;
+            if (head == null) {
+                head = new ListNode(sum);
+                p = head;
+            }else {
+                ListNode node = new ListNode(sum);
+                node.next = null;
+                p.next = node;
+                p = p.next;
+            }
+        }
+        return head;
     }
 
     /**
      * 链表
      */
-    public class ListNode {
+    public static class ListNode {
         int val;
         ListNode next;
 
@@ -25,7 +58,62 @@ public class AddTwoNum {
         }
     }
 
-    public static void main(String[] args) {
 
+
+    /**
+     * 根据整数创建链表
+     * @param num
+     * @return
+     */
+    public static ListNode createListNode(int num){
+        String s1 = String.valueOf(num);
+        StringBuilder sb = new StringBuilder(s1);
+        String s2 = sb.reverse().toString();
+        int length = s2.length();
+        ListNode p = null;
+        ListNode head = null;
+        for (int i=0;i<length;i++) {
+            int n = Integer.valueOf("" + s2.charAt(i));
+            if (head == null) {
+                head = new ListNode(n);
+                p = head;
+            } else {
+                ListNode node = new ListNode(n);
+                p.next = node;
+                p=p.next;
+            }
+        }
+        return head;
+    }
+
+    /**
+     * 打印链表
+     * @param node
+     */
+    public static void  displayListNode(ListNode node){
+        if (node == null) {
+            return;
+        }
+        StringBuilder sb = new StringBuilder();
+        ListNode p = node;
+
+        while (p != null) {
+            int num = p.val;
+            sb.append(num);
+            p = p.next;
+        }
+        System.out.println(sb.toString());
+
+    }
+
+
+    public static void main(String[] args) {
+        //243,564
+        //ListNode list1 = createListNode(342);
+        ListNode list1 = createListNode(1);
+        //ListNode list2 = createListNode(465);
+        ListNode list2 = createListNode(99);
+        ListNode list3 = addTwoNumbers(list1,list2);
+        displayListNode(list3);
     }
 }
