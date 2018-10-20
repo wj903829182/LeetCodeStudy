14511f855cf297b9a2e224097eceeaac38189f91
jack <wj903829182@sina.cn>
2018-10-20 17:32:27 +0800

    最大长度不重复的字符串

--- src/main/java/com/jack/algorithm/LongestSubstring.java
+++ src/main/java/com/jack/algorithm/LongestSubstring.java
@@ -14,11 +14,34 @@ public class LongestSubstring {
      * @param s
      * @return
      */
-    public int lengthOfLongestSubstring(String s) {
-        return 0;
+    public static int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        int maxLength =0;
+        int length = s.length();
+        String str = "";
+        for (int i= 0;i<length;i++) {
+            String s1 = String.valueOf(s.charAt(i));
+            if (s1.length() > max) {
+                max = s1.length();
+            }
+            for (int j=i+1;j<length;j++) {
+                String s2 = String.valueOf(s.charAt(j));
+                if (!s1.contains(s2)) {
+                    s1 = s1 + s2;
+                    maxLength = s1.length();
+                    if (maxLength > max) {
+                        max = maxLength;
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+        return max;
     }
 
     public static void main(String[] args) {
-
+        int length = lengthOfLongestSubstring("pwwkew");
+        System.out.println("length="+length);
     }
 }
