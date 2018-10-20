065ff3146836eb36b7ab32a3acdc2c139f87ec4b
jack <wj903829182@sina.cn>
2018-10-20 10:13:31 +0800

    两个数相加

---
+++ src/main/java/com/jack/algorithm/LongestSubstring.java
@@ -0,0 +1,24 @@
+package com.jack.algorithm;
+
+/**
+ * create by jack 2018/10/20
+ *
+ * @auther jack
+ * @date: 2018/10/20 10:10
+ * @Description:
+ */
+public class LongestSubstring {
+    /**
+     * 题目描述：
+     * https://leetcode.com/problems/longest-substring-without-repeating-characters/
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring(String s) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
