40b2a99fbdda3f73d4e968ae324bb3c4966ab9f8
jack <wj903829182@sina.cn>
2018-11-04 20:52:20 +0800

    判断括号是否匹配

---
+++ src/main/java/com/jack/entity/ListNode.java
@@ -0,0 +1,17 @@
+package com.jack.entity;
+
+/**
+ * create by jack 2018/11/4
+ *
+ * @author jack
+ * @date: 2018/11/4 11:59
+ * @Description:
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) {
+        val = x;
+    }
+
+}
--- src/main/java/com/jack/util/ListUtil.java
+++ src/main/java/com/jack/util/ListUtil.java
@@ -1,5 +1,8 @@
 package com.jack.util;
 
+import com.jack.algorithm.MergeTwoSortedLists;
+import com.jack.entity.ListNode;
+
 /**
  * create by jack 2018/11/4
  *
@@ -8,4 +11,40 @@ package com.jack.util;
  * @Description:
  */
 public class ListUtil {
+    /**
+     * 创建链表
+     * @param nums
+     * @return
+     */
+    public static ListNode createListNode(int[] nums){
+        ListNode head=null;
+        ListNode p = head;
+        for (int i=0;i<nums.length;i++) {
+            int num = nums[i];
+            ListNode node = new ListNode(num);
+            if (head == null) {
+                head = node;
+                p = head;
+            } else {
+                p.next = node;
+                p = node;
+            }
+        }
+        return head;
+    }
+
+    /**
+     * 打印链表
+     * @param nodes
+     */
+    public static void displayListNode(ListNode nodes){
+        ListNode p = nodes;
+        while (p != null) {
+            int num = p.val;
+            System.out.print(num+"->");
+            p=p.next;
+        }
+        System.out.println("");
+    }
+
 }
