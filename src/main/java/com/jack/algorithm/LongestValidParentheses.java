07dea75b73900e861fb213b8cf13a0f324b6dd25
jack <wj903829182@sina.cn>
2019-05-04 20:30:43 +0800

    最大匹配括号长度Longest Valid Parentheses

--- src/main/java/com/jack/algorithm/LongestValidParentheses.java
+++ src/main/java/com/jack/algorithm/LongestValidParentheses.java
@@ -1,5 +1,7 @@
 package com.jack.algorithm;
 
+import java.util.Stack;
+
 /**
  * create by jack 2019/5/4
  *
@@ -15,11 +17,31 @@ public class LongestValidParentheses {
      * @return
      */
     public static int longestValidParentheses(String s) {
-        return 0;
+        int maxans = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            //如果是左括号，则入栈
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                //如果是右括号则出栈
+                stack.pop();
+                if (stack.empty()) {
+                    //如果栈为空了，则记下当前的下标
+                    stack.push(i);
+                } else {
+                    //如果不为空，则获取最大长度
+                    maxans = Math.max(maxans, i - stack.peek());
+                }
+            }
+        }
+        return maxans;
     }
 
     public static void main(String[] args) {
-        String s = "(()";
+        //String s = "(()";
+        String s = ")()())";
         int rs =longestValidParentheses(s);
         System.out.println("rs="+rs);
     }
