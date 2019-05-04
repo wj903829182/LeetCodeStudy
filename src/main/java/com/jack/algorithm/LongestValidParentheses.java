1dfea48e5a8c2c2673148caa1b19ef08c4ca1641
jack <wj903829182@sina.cn>
2019-05-04 20:07:42 +0800

    Longest Valid Parentheses

---
+++ src/main/java/com/jack/algorithm/LongestValidParentheses.java
@@ -0,0 +1,27 @@
+package com.jack.algorithm;
+
+/**
+ * create by jack 2019/5/4
+ *
+ * @author jack
+ * @date: 2019/5/4 19:44
+ * @Description:
+ */
+public class LongestValidParentheses {
+    /**
+     * 32-题目描述：
+     * https://leetcode.com/problems/longest-valid-parentheses/
+     * @param s
+     * @return
+     */
+    public static int longestValidParentheses(String s) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        String s = "(()";
+        int rs =longestValidParentheses(s);
+        System.out.println("rs="+rs);
+    }
+
+}
